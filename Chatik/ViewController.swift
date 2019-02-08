8119862da5154b19c2f726211f42cfc0ccce6d21
Hiii Power <danila@macbook-pro-hiii-power.local>
2019-02-08 11:50:26 +0300

    Initial Commit

---
+++ Chatik/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Chatik
+//
+//  Created by Hiii Power on 08.02.19.
+//  Copyright © 2019 Hiii Power. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
