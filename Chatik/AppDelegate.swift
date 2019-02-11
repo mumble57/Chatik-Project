36e41a185efb8d11405db59dd9ad075d4bc57771
Armen Aydinyan <mumble57558@ya.ru>
2019-02-11 21:35:21 +0300

    Гы

--- Chatik/AppDelegate.swift
+++ Chatik/AppDelegate.swift
@@ -13,32 +13,84 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-
+    enum States : String {
+        case NotRunning = "Not running", Active = "Active", Inactive = "Inactive", Background = "Background"
+    }
+    
+    enum LogState{
+        case On
+        case Off
+    }
+    
+    var presentState = States.Background
+    var previousState = States.Inactive
+    
+    //Turn logs on/off here
+    var logState = LogState.Off
+    //var logState = LogState.Off
+    
+    var output = String()
+    func lifeCycle(previousState: String, presentState: String, methodName: String) -> String {
+        switch logState {
+        case .On:
+            output = "Application moved from state " + previousState + " to state " + presentState + " : " + methodName
+            return output
+        case .Off:
+            output = ""
+            return output
+        }
+        
+    }
+    
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        previousState = presentState
+        presentState = .Active
+        print(lifeCycle(previousState: previousState.rawValue, presentState: presentState.rawValue, methodName: "didFinishLaunchingWithOptions"))
+        
         return true
+
     }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
         // Use this method to pause ongoing tasks, disable timers, and invalidate graphics rendering callbacks. Games should use this method to pause the game.
+        previousState = presentState
+        presentState = .Inactive
+        print(lifeCycle(previousState: previousState.rawValue, presentState: presentState.rawValue, methodName: "applicationWillResignActive"))
     }
 
     func applicationDidEnterBackground(_ application: UIApplication) {
         // Use this method to release shared resources, save user data, invalidate timers, and store enough application state information to restore your application to its current state in case it is terminated later.
         // If your application supports background execution, this method is called instead of applicationWillTerminate: when the user quits.
+        previousState = presentState
+        presentState = .Background
+        print(lifeCycle(previousState: previousState.rawValue, presentState: presentState.rawValue, methodName: "applicationDidEnterBackground"))
     }
 
     func applicationWillEnterForeground(_ application: UIApplication) {
         // Called as part of the transition from the background to the active state; here you can undo many of the changes made on entering the background.
+        previousState = presentState
+        presentState = .Inactive
+        print(lifeCycle(previousState: previousState.rawValue, presentState: presentState.rawValue, methodName: "applicationWillEnterForeground"))
+
     }
 
     func applicationDidBecomeActive(_ application: UIApplication) {
         // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+       // print(lifeCycle(previousState: "Background/Inactive", presentState: "Active", methodName: "applicationDidBecomeActive"))
+        previousState = presentState
+        presentState = .Active
+        print(lifeCycle(previousState: previousState.rawValue, presentState: presentState.rawValue, methodName: "applicationDidBecomeActive"))
+
     }
 
     func applicationWillTerminate(_ application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
+       // print(lifeCycle(previousState: "Inactive/Active", presentState: "Not Running", methodName: "applicationDidBecomeActive"))
+        previousState = presentState
+        presentState = .Inactive
+        print(lifeCycle(previousState: previousState.rawValue, presentState: presentState.rawValue, methodName: "applicationWillTerminate"))
     }
 
 
--- Chatik/ViewController.swift
+++ Chatik/ViewController.swift
@@ -10,16 +10,83 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    enum States : String {
+        case viewDidNotExist = "view did not exist",loadView = "loadView", viewDidLoad = "viewDidLoad", viewWillAppear = "viewWillAppear",  viewDidAppear = "viewDidAppear", viewWillLayoutSubviews = "viewWillLayoutSubviews", viewDidLayoutSubviews = "viewDidLayoutSubviews", viewWillDisappear = "viewWillDisappear", viewDidDisappear = "viewDidDisappear"
+    }
+    
+    enum LogState{
+        case On
+        case Off
+    }
+    
+    var presentState = States.viewDidNotExist
+    var previousState = States.viewDidNotExist
+    
+    //Turn logs on/off here
+    var logState = LogState.On
+    //var logState = LogState.Off
+    
+    var output = String()
+    func lifeCycle(previousState: String, presentState: String) -> String {
+        switch logState {
+        case .On:
+            output = "Application did  " + previousState + " to state " + presentState
+            return output
+        case .Off:
+            output = ""
+            return output
+        }
+        
+    }
+    override func loadView() {
+        super.loadView()
+        previousState = presentState
+        presentState = .loadView
+        print(lifeCycle(previousState: previousState.rawValue, presentState: presentState.rawValue))
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
+        previousState = presentState
+        presentState = .viewDidLoad
+        print(lifeCycle(previousState: previousState.rawValue, presentState: presentState.rawValue))
         // Do any additional setup after loading the view, typically from a nib.
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(true)
+        previousState = presentState
+        presentState = .viewWillAppear
+        print(lifeCycle(previousState: previousState.rawValue, presentState: presentState.rawValue))
     }
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(true)
+        previousState = presentState
+        presentState = .viewDidAppear
+        print(lifeCycle(previousState: previousState.rawValue, presentState: presentState.rawValue))
+    }
+    override func viewWillLayoutSubviews() {
+        super.viewWillLayoutSubviews()
+        previousState = presentState
+        presentState = .viewWillLayoutSubviews
+        print(lifeCycle(previousState: previousState.rawValue, presentState: presentState.rawValue))
+    }
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        previousState = presentState
+        presentState = .viewDidLayoutSubviews
+        print(lifeCycle(previousState: previousState.rawValue, presentState: presentState.rawValue))
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(true)
+        previousState = presentState
+        presentState = .viewWillDisappear
+        print(lifeCycle(previousState: previousState.rawValue, presentState: presentState.rawValue))
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(true)
+        previousState = presentState
+        presentState = .viewDidDisappear
+        print(lifeCycle(previousState: previousState.rawValue, presentState: presentState.rawValue))
+    }
 
 }
 
